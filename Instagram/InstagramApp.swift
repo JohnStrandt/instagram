16a68a0b759df3273f230239e8f4edd157f78cf5
JohnStrandt <jstrandt@hotmail.com>
2024-02-28 16:08:40 -0600

    Initial Commit

---
+++ Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by John Strandt on 2/28/24.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
